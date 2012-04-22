5867806f5bf08a38e1b52399503223323984df65
Simon Cadman <src@niftiestsoftware.com>
2012-04-22 17:21:55 +0100

    Added ebuild creator

---
+++ build-ebuild.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+set -e
+if [[ $# -lt 2 ]]; then
+   echo "USAGE: ./build-ebuild.sh name gitrepo [commit]"
+   exit
+fi
+
+export start="`pwd`"
+export name="$1"
+export gitrepo="$2"
+export commit="$3"
+export date="`date +%Y%m%d`"
+
+rm -rf /tmp/buildpackage/$name-$date
+mkdir -p /tmp/buildpackage/$name-$date
+cd /tmp/buildpackage/$name-$date
+git clone $gitrepo .
+if [[ $commit != ""  ]]; then
+	git checkout $commit
+else
+	commit="`cat /tmp/buildpackage/$name-$date/.git/refs/heads/master`"
+fi
+rm -rf /tmp/buildpackage/$name-$date/.git
+#cat packages/gentoo/$name.ebuild > $start/$name-$date.ebuild
+sed -i 's/ugly/dsd/g' /tmp/buildpackage/$name-$date/packages/gentoo/$name.ebuild
+echo "Ebuild created"
