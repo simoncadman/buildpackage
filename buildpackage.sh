6a53f328bfe6df1e89b2e01e062f9dc3d8ee2cfa
Simon Cadman <src@niftiestsoftware.com>
2012-04-22 13:41:17 +0100

    Use buildpackage dir in tmp

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 if [[ $# -lt 2 ]]; then
    echo "USAGE: ./buildpackage name gitrepo [commit]"
    exit
@@ -9,17 +10,19 @@ export gitrepo="$2"
 export commit="$3"
 export date="`date +%Y%m%d`"
 
-rm -rf /tmp/$name-$date
-mkdir -p /tmp/$name-$date
-cd /tmp/$name-$date
+mkdir -p /tmp/buildpackage
+rm -rf /tmp/buildpackage/$name-$date
+mkdir -p /tmp/buildpackage/$name-$date
+cd /tmp/buildpackage/$name-$date
 git clone $gitrepo . 
 if [[ $commit != ""  ]]; then
 	git checkout $commit
 fi
-rm -rf /tmp/$name-$date/.git
-cd /tmp/
+rm -rf /tmp/buildpackage/$name-$date/.git
+cd /tmp/buildpackage/
 tar cjf $name\_$date.orig.tar.bz2 $name-$date
 cd $name-$date
 mv packages/debian debian
 rm -rf packages
 debuild -S
+echo "Files in /tmp/buildpackage/ , run dput changes file"
