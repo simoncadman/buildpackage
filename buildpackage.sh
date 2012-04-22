425db232b07d0732c13c8b593894a2976d258229
Simon Cadman <src@niftiestsoftware.com>
2012-04-22 12:40:42 +0100

    Commit is now optional, rely on packages dir

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
-echo $#
-if [[ $# -lt 3 ]]; then
-   echo "USAGE: ./buildpackage "
+if [[ $# -lt 2 ]]; then
+   echo "USAGE: ./buildpackage name gitrepo [commit]"
    exit
 fi
+
+export name="$1"
+export gitrepo="$2"
+export commit="$3"
+export date="`date +%Y%m%d`"
+
+rm -rf /tmp/$name-$date
+mkdir -p /tmp/$name-$date
+cd /tmp/$name-$date
+git clone $gitrepo . 
+if [[ $commit != ""  ]]; then
+	git checkout $commit
+fi
+rm -rf /tmp/$name-$date/.git
+mv packages/debian debian
