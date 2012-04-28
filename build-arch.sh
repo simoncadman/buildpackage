968b7c9cf4c238887c12295305c57bf0892c3a4f
Simon Cadman <src@niftiestsoftware.com>
2012-04-28 21:01:45 +0100

    Added script to build arch packages

---
+++ build-arch.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+set -e
+if [[ $# -lt 2 ]]; then
+   echo "USAGE: ./build-arch.sh name gitrepo [commit]"
+   exit
+fi
+
+export name="$1"
+export gitrepo="$2"
+export commit="$3"
+export date="`date +%Y%m%d`"
+
+rm -rf /tmp/buildpackage/$name-$date
+mkdir -p /tmp/buildpackage/$name-$date/
+cd /tmp/buildpackage/
+git clone $gitrepo $name-$date
+cd $name-$date 
+if [[ $commit != ""  ]]; then
+	git checkout $commit
+fi
+cd packages/arch
+makepkg
+echo "Files in `pwd` , upload `ls *.tar.xz` "
