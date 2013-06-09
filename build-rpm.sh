fc9512d4e8e37d267da0f8f7e646a1881a00cd55
Simon Cadman <src@niftiestsoftware.com>
2013-06-09 19:54:45 +0100

    Fixed rpm spec name

--- build-rpm.sh
+++ build-rpm.sh
@@ -34,5 +34,5 @@ rm -rf /root/rpmbuild
 mkdir -p /root/rpmbuild/SOURCES
 mv $name-$date.tar.bz2 /root/rpmbuild/SOURCES/
 cd ..
-rpmbuild -ba SPECS/cupscloudprint.spec --target $arch
+rpmbuild -ba SPECS/$name.spec --target $arch
 echo "Files in /root/rpmbuild/RPMS , copy to /root/niftyreporpm/$arch/ , run upload and then upload to s3. "
