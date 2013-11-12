0d36469d90668e379a214823a5ea90634470af78
Simon Cadman <src@niftiestsoftware.com>
2013-11-12 23:19:16 +0000

    Show correct output filename

--- build-ebuild.sh
+++ build-ebuild.sh
@@ -24,4 +24,4 @@ rm -rf /tmp/buildpackage/$name-$date/.git
 mkdir -p $start/out/
 #cat packages/gentoo/$name.ebuild > $start/$name-$date.ebuild
 sed  "s/EGIT_COMMIT=\".*\"/EGIT_COMMIT=\"$commit\"/g" /tmp/buildpackage/$name-$date/packages/gentoo/$name.ebuild > $start/out/$name-$date.ebuild
-echo "Ebuild created as $start/$name-$date.ebuild"
+echo "Ebuild created as $start/out/$name-$date.ebuild"
