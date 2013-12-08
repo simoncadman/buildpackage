08097c66546d42de0a83a464c473a21b35cc2a35
Simon Cadman <src@niftiestsoftware.com>
2013-12-08 02:34:29 +0000

    ADded rpm test script

---
+++ test-rpm.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+if [[ $# -lt 3 ]]; then
+   echo "USAGE: ./test-rpm.sh name category packagetestscript [package options]"
+   exit 1
+fi
+
+if [[ "`whoami`" != 'root'  ]]; then
+   echo "Need to run as root, will install package and test newly created rpm"
+   exit 1
+fi
+
+export start="`pwd`"
+export name="$1"
+export date="`date +%Y%m%d`"
+export category="$2"
+export testscript="$3"
+
+yum install -y /$HOME/rpmbuild/RPMS/noarch/$name-$date-1.noarch.rpm
+$testscript $@
