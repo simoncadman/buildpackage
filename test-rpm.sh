932d5ac0738f996068b75acf0b8c17c18c1544a7
Simon Cadman <src@niftiestsoftware.com>
2016-04-09 00:17:37 +0100

    Dont use date param in testing

--- test-rpm.sh
+++ test-rpm.sh
@@ -13,15 +13,11 @@ fi
 
 export start="`pwd`"
 export name="$1"
-export date="`date +%Y%m%d`"
 export category="$2"
 export testscript="$3"
 export uninstalledtestscript="$4"
-if [[ $6 != "" ]]; then
-        date="$6"
-fi
 
-yum install -y /$HOME/rpmbuild/RPMS/noarch/$name-$date-1.noarch.rpm
+yum install -y /$HOME/rpmbuild/RPMS/noarch/$name-*-1.noarch.rpm
 if [[ $testscript != "" ]]; then
     $testscript $@
 fi
