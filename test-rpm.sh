482de26eb32b0f8713de3b9b270d2736b392b53d
Simon Cadman <src@niftiestsoftware.com>
2016-04-13 22:11:52 +0100

    Use dnf as package manager if available for rpm test

--- test-rpm.sh
+++ test-rpm.sh
@@ -17,13 +17,20 @@ export category="$2"
 export testscript="$3"
 export uninstalledtestscript="$4"
 
-yum install -y /$HOME/rpmbuild/RPMS/noarch/$name-*-1.noarch.rpm
+PACKAGEMANAGER="yum"
+
+if which dnf > /dev/null 2>&1; then
+	PACKAGEMANAGER="dnf"
+	echo "Using dnf for package manager"
+fi
+
+$PACKAGEMANAGER install -y /$HOME/rpmbuild/RPMS/noarch/$name-*-1.noarch.rpm
 if [[ $testscript != "" ]]; then
     $testscript $@
 fi
 if [[ $uninstalledtestscript != "" ]]; then
     cp $uninstalledtestscript /tmp/test-remove
-    yum remove -y $name
+    $PACKAGEMANAGER remove -y $name
     /tmp/test-remove $@
     unlink /tmp/test-remove
 fi
