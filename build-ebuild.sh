01e9b54a453d603dcd527c9d1d13858085bb2570
Simon Cadman <src@niftiestsoftware.com>
2016-04-14 21:56:46 +0100

    Use workspace dir for ebuild

--- build-ebuild.sh
+++ build-ebuild.sh
@@ -1,38 +1,24 @@
 #! /bin/bash
 set -e
 if [[ $# -lt 2 ]]; then
-   echo "USAGE: ./build-ebuild.sh name gitrepo [commit]"
+   echo "USAGE: ./build-ebuild.sh name workspace"
    exit 1
 fi
 
 export start="`pwd`"
 export name="$1"
-export gitrepo="$2"
-export commit="$3"
+export workspace="$2"
 export date="`date +%Y%m%d`"
-export isbranch="$5"
-
-if [[ $4 != "" ]]; then
-    date="$4"
-fi
 
 rm -rf /tmp/buildpackage/$name-$date
 mkdir -p /tmp/buildpackage/$name-$date
 cd /tmp/buildpackage/$name-$date
-git clone $gitrepo .
-if [[ $commit != ""  ]]; then
-	git checkout $commit
-else
-	commit="`cat /tmp/buildpackage/$name-$date/.git/refs/heads/master`"
-fi
-rm -rf /tmp/buildpackage/$name-$date/.git
-rm -rf $start/out/
-mkdir -p $start/out/
+commit="`cat $WORKSPACE/.git/refs/heads/master`"
+rm -rf /$WORKSPACE/.git
+rm -rf $start/ebuilds/
+mkdir -p $start/ebuilds/
 
-if [[ $isbranch == "branch" ]]; then
-    sed  "s/EGIT_COMMIT=\".*\"/EGIT_BRANCH=\"$commit\"/g" /tmp/buildpackage/$name-$date/packages/gentoo/$name.ebuild > $start/out/$name-$date.ebuild    
-else
-    sed  "s/EGIT_COMMIT=\".*\"/EGIT_COMMIT=\"$commit\"/g" /tmp/buildpackage/$name-$date/packages/gentoo/$name.ebuild > $start/out/$name-$date.ebuild    
-fi
+sed  "s/EGIT_COMMIT=\".*\"/EGIT_COMMIT=\"$commit\"/g" /tmp/buildpackage/$name-$date/packages/gentoo/$name.ebuild > $start/ebuilds/$name-$date.ebuild
+ebuild $start/ebuilds/$name-$date.ebuild  digest
 
-echo "Ebuild created as $start/out/$name-$date.ebuild"
+echo "Ebuild created as $start/ebuilds/$name-$date.ebuild"
