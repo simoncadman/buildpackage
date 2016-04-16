a7f667f130b3941ac348f94091d17b441e5bee44
Simon Cadman <src@niftiestsoftware.com>
2016-04-16 16:35:47 +0100

    Create local portage repo correctly

--- build-ebuild.sh
+++ build-ebuild.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 
 if [[ $# -lt 2 ]]; then
-   echo "USAGE: ./build-ebuild.sh name workspace"
+   echo "USAGE: ./build-ebuild.sh name workspace category"
    exit 1
 fi
 
@@ -11,6 +11,7 @@ export start="`pwd`"
 export name="$1"
 export WORKSPACE="$2"
 export date="`date +%Y%m%d`"
+export category="$3"
 
 cd "$WORKSPACE"
 commit="`git rev-parse HEAD`"
@@ -20,6 +21,11 @@ rm -rf $start/ebuilds/
 mkdir -p $start/ebuilds/
 
 sed  "s/EGIT_COMMIT=\".*\"/EGIT_COMMIT=\"$commit\"/g" "$WORKSPACE/packages/gentoo/$name.ebuild" > "$start/ebuilds/$name-$date.ebuild"
-ebuild $start/ebuilds/$name-$date.ebuild  digest
+mkdir -p /usr/local/portage/$category/$name/
+echo 'masters = gentoo' > /usr/local/portage/layout.conf
+cp "$start/ebuilds/$name-$date.ebuild" /usr/local/portage/$category/$name/
+ebuild /usr/local/portage/$category/$name/$name-$date.ebuild digest
+mkdir -p $start/ebuilds/$category/$name
+cp usr/local/portage/$category/$name/* $start/ebuilds/$category/$name
 
-echo "Ebuild created as $start/ebuilds/$name-$date.ebuild"
+echo "Ebuild created in $start/ebuilds/"
