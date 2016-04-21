3d55ba9d18d329c181dd6a71d2238048ee31aa37
Simon Cadman <src@niftiestsoftware.com>
2016-04-21 20:05:48 +0100

    Use workspace dir for darwin

--- build-darwin.sh
+++ build-darwin.sh
@@ -2,32 +2,19 @@
 
 set -e
 if [[ $# -lt 2 ]]; then
-  echo "USAGE: $0 name gitrepo [commit]"
+  echo "USAGE: $0 name workspace"
   exit 1
 fi
 
 export start=$PWD
 export name="$1"
-export gitrepo="$2"
-export commit="$3"
+export workspace="$2"
 export date="`date +%Y%m%d`"
 
-if [[ $4 != "" ]]; then
-    date="$4"
-fi
-
 export tmp="/tmp/buildpackage-$date"
-export srcdir="$tmp/$name-src"
 export builddir="$tmp/$name-build"
 
-sudo rm -rf "$srcdir" "$builddir"
-mkdir -p "$srcdir" "$builddir"
-cd $srcdir
-
-git clone $gitrepo . 
-if [[ $commit != "" ]]; then
-  git checkout $commit
-fi
+cd $workspace
 
 cupsgroup="_lp" ./configure --prefix=/usr
 sudo make install DESTDIR="$builddir"
@@ -39,7 +26,7 @@ pkgbuild \
   --identifier com.niftiestsoftware.ccp \
   --ownership preserve \
   --version "$date" \
-  --scripts "$srcdir/packages/darwin" \
+  --scripts "$workspace/packages/darwin" \
   "$start/$name-$date.pkg"
 
 sudo rm -rf "$tmp"
