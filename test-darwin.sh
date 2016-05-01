03ab32b11456b36e055f7d784e379868a7d8c333
Simon Cadman <src@niftiestsoftware.com>
2016-05-01 21:12:01 +0100

    Take package name as param

--- test-darwin.sh
+++ test-darwin.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ $# -lt 2 ]]; then
-   echo "USAGE: ./test-darwin.sh name category [packagetestscript] [package options]"
+   echo "USAGE: ./test-darwin.sh name category [packagetestscript] [package options] [packagename]"
    exit 1
 fi
 
@@ -17,10 +17,11 @@ export name="$1"
 export category="$2"
 export testscript="$3"
 export uninstalledtestscript="$4"
+export packagename="$5"
 
 trap 'echo exiting; tail -n100 /var/log/install.log' exit
 
-installer -dumplog -verbose -pkg cupscloudprint-*.pkg -target /
+installer -dumplog -verbose -pkg ${name}-*.pkg -target /
 if [[ $testscript != "" ]]; then
     $testscript $@
 fi
@@ -28,7 +29,8 @@ if [[ $uninstalledtestscript != "" ]]; then
     cp $uninstalledtestscript /tmp/test-remove
     cd "$start"
     pkgutil --pkgs
-    pkgutil --forget --unlink cupscloudprint-*.pkg
+    pkgutil --forget --unlink $packagename
     /tmp/test-remove $@
     unlink /tmp/test-remove
+    pkgutil --pkgs
 fi
